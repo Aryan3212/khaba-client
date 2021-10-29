26e039ba59896bbb397ba9acbfdc0a3ce605fc51
mushaidul <mushaidul.islam@g.bracu.ac.bd>
2021-10-29 16:20:09 +0600

    ddsf

--- src/components/DataEntry/DataShowTable/DataShowTable.js
+++ src/components/DataEntry/DataShowTable/DataShowTable.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./DataShowTable.scss";
 
 import DataShowItem from "./DataShowItem/DataShowItem";
+import Axios from "axios";
 
 export default function DataShowTable() {
+    const [burger, setBurger] = useState([])
+    const [cream, setCream] = useState([])
+    const [fries, setFries] = useState([])
+    const [rice, setRice] = useState([])
     const a = [
         [
             ["burger", 12.8, 2, "21-10-2021"],
@@ -62,6 +67,38 @@ export default function DataShowTable() {
         ],
     ];
 
+    function callAPI(name) {
+        Axios.get('http://localhost:4200/getWaste',
+            {
+                params: {
+                    name: name
+                }
+            }
+        ).then((response) => {
+            if (response.status === 200) {
+
+                if (name === 'Burger') setBurger(response.data.data.days)
+                if (name === 'Ice Cream') setCream(response.data.data.days)
+                if (name === 'French Fries') setFries(response.data.data.days)
+                if (name === 'Rice') setRice(response.data.data.days)
+
+            }
+            else {
+
+                console.log('error at get')
+            }
+        });
+    }
+
+    useEffect(() => {
+        callAPI('Burger')
+        callAPI('Ice Cream')
+        callAPI('French Fries')
+        callAPI('Rice')
+    }, []);
+
+
+
     return (
         <div className="show-table">
             <h2>Overview of Food Wasted in the last 10 days</h2>
@@ -71,29 +108,36 @@ export default function DataShowTable() {
                     <h4>Produced(kg)</h4>
                     <h4>Wasted</h4>
                     <h4>Date</h4>
+                    <DataShowItem itemList={burger} name='Burger' />
                 </div>
                 <div>
                     <h4>Item</h4>
                     <h4>Produced(kg)</h4>
                     <h4>Wasted</h4>
                     <h4>Date</h4>
+                    <DataShowItem itemList={cream} name='Ice Cream' />
                 </div>
                 <div>
                     <h4>Item</h4>
                     <h4>Produced(kg)</h4>
                     <h4>Wasted</h4>
                     <h4>Date</h4>
+                    <DataShowItem itemList={fries} name='French Fries' />
                 </div>
                 <div>
                     <h4>Item</h4>
                     <h4>Produced(kg)</h4>
                     <h4>Wasted</h4>
                     <h4>Date</h4>
+                    <DataShowItem itemList={rice} name='Rice' />
                 </div>
             </div>
-            {a.map((elem) => (
-                <DataShowItem itemList={elem} />
-            ))}
+
+
+
+
+
+
         </div>
     );
 }
